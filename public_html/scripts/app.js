c536c2258beede93b4ef792d0d4a2930cfefd720
Pierre <mousset.p@husky.neu.edu>
2021-01-07 15:53:55 +0100

    (major) added sections and scripts folders

--- public_html/scripts/app.js
+++ public_html/scripts/app.js
@@ -1,3 +1,17 @@
+window.onload = function() {
+    fetch("sections/about.html").then(data => data.text()).then(data => {
+        document.querySelector("#about").innerHTML = data
+      })
+      fetch("sections/projects.html").then(data => data.text()).then(data => {
+        document.querySelector("#projects").innerHTML = data
+      })
+      fetch("sections/contact.html").then(data => data.text()).then(data => {
+        document.querySelector("#contact").innerHTML = data
+      })
+}
+  
+
+
 function toggleNav(){
         items = document.querySelectorAll(".items");
         for (i = 0; i < items.length; ++i) {
